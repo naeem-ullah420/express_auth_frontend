823cab21464a5b450453e516c748c4f95e2683d7
era-tech=420 <eratech420@gmail.com>
2023-10-07 15:46:06 +0500

    sign route integrated with backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, InputGroup } from 'react-bootstrap';
+import { Container, Row, Col, Form, Image } from 'react-bootstrap';
+import SignUp from './pages/SignUpPage';
+// import { ArrowRight, Person, PersonFill } from 'react-bootstrap-icons';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Toaster position="top-right"/>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/signUp' element={<SignUp/>}></Route>
+        </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { Button, InputGroup } from 'react-bootstrap';
+import { Container, Row, Col, Form, Image } from 'react-bootstrap';
+// import { ArrowRight, Person, PersonFill } from 'react-bootstrap-icons';
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast';
+
+function SignUp() {
+const failedNotify = (m) => toast.error(m)
+const successfulNotify = (m) => toast.success(m)
+
+
+  const [signUpPayload, setSignUpPayload] = useState({
+    "email":"",
+    "name":"",
+    "password":"",
+    "confirm_password":""
+  })
+
+  const handleChange = (e) => {
+    setSignUpPayload({
+        ...signUpPayload,
+        [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log("signUpPayload: ",signUpPayload)
+    // submit signup request
+    try {
+        const response = await axios({
+            method: 'post',
+            url: 'http://127.0.0.1:8000/api/auth/signup',
+            data : signUpPayload
+        })
+        console.log("response: ", response)
+        successfulNotify("SignUp Successful")
+    } catch (error) {
+        failedNotify(error.response.data.message)
+    }
+  }
+
+  return (
+    <Container fluid >
+      <Row style={{
+        height: "100vh"
+      }}>
+        <Col className="pt-4 px-5" md={4}>
+          <h2 className='text-center'>Sign Up</h2>
+          <Form className='pt-4' onSubmit={handleSubmit}>
+            <Form.Control type='text' placeholder='Your Name' name='name' className='mt-4' value={signUpPayload.name} onChange={handleChange} />
+            <Form.Control type='email' placeholder='Your Email' name="email" className='mt-4' value={signUpPayload.email} onChange={handleChange}/>
+            <Form.Control type='password' placeholder='Password' name="password" className='mt-4' value={signUpPayload.password} onChange={handleChange}/>
+            <Form.Control type='password' placeholder='Repeat Password' name='confirm_password' value={signUpPayload.confirm_password} className='mt-4' onChange={handleChange} />
+            <Form.Check label="I agree all statements in Terms of Services" className='mt-4'></Form.Check>
+            <Button variant="primary" type='submit' className='mt-4 align-self-center'>Register</Button>
+          </Form>
+        </Col>
+        <Col md={8} className="d-flex justify-content-center align-items-center">
+          <Image width="400px" height="400px" src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default SignUp
